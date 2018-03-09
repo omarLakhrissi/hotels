da8376ad7b00f12cdf157d673fefc06bb9b6cf82
omar lakhrissi <omar.lakhrissi@insa-cvl.fr>
2018-03-09 19:19:18 +0100

    nouveau fichier : docs/bdd.ods nouveau fichier : site_web/index.php

---
+++ site_web/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <meta charset="utf-8" />
+
+        <title>Ceci est une page PHP de test</title>
+
+    </head>
+
+    <body>
+        <?php
+        /*$coordonnees = array (
+            'prenom' => 'François',
+            'nom' => 'Dupont',
+            'adresse' => '3 Rue du Paradis',
+            'ville' => 'Marseille'
+        );
+        echo '<pre>';
+        print_r($coordonnees);
+	echo '</pre>'0;*/
+
+	phpinfo();
+        ?>       
+    </body>
+	
+  
+</html>
